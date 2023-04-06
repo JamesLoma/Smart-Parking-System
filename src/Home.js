aecbfbf6a0529a109990d4ece02348a2b93e53b6
JamesLoma <jamesloma80@gmail.com>
2023-04-06 14:18:00 +0300

    changes

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    let name = 'mario';
+
+    const handleClick= (e) => {
+        name = 'luigoi';
+        console.log(name);
+    }
+
     return ( 
         <div className="Home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>click me</button>
         </div>
         
      );
