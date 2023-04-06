6fefb1235066d60835c99d111dc657fbe666563b
JamesLoma <jamesloma80@gmail.com>
2023-04-06 15:28:19 +0300

    using state

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,18 @@
+import { useState } from "react";
 const Home = () => {
-    let name = 'mario';
+    //let name = 'mario';
+    const [name, setName] = useState('mario');  
+    const [age, setAge] = useState(25);
 
-    const handleClick= (e) => {
-        name = 'luigoi';
-        console.log(name);
+    const handleClick= () => {
+        setName('luigi');
+        setAge(30);
     }
 
     return ( 
         <div className="Home">
             <h2>Home Page</h2>
+            <p>{ name } is {age} age</p>
             <button onClick={handleClick}>click me</button>
         </div>
         
